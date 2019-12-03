36590dd2574a0c1b3054e5fad115d9a2ef01e036
ENS-ITEM <contact@numerizen.com>
2019-12-03 11:15:55 +0100

    Added link to expand or retract the tree

--- views/public/index/affichecollections.php
+++ views/public/index/affichecollections.php
@@ -1,8 +1,8 @@
 <?php
-  echo head(array('title' => 'Arborescence du corpus', 'bodyclass' => 'collections browse')); ?>
+echo head(array('title' => 'Arborescence du corpus', 'bodyclass' => 'collections browse')); ?>
   
 <style>
-div.notices {
+div.notices, div.collections {
   display:none;
 }
 .montrer {
@@ -15,18 +15,30 @@ div.notices {
 #titre-arbo {
   font-weight: bold;
 }
+.tout {
+  cursor:pointer;
+}
 </style>
 <script>
   $ = jQuery;
   $(document).ready(function() {
     $('.montrer').click(function() {
-      $(this).parent().next('.notices, .collections').toggle();
+      $(this).parent().nextAll('div.notices, div.collections').toggle();
+    });
+    $('.tout').click(function() {
+      $('div.notices, div.collections').toggle();
+      if ($(this).html() == 'Tout replier') {
+        $(this).html('Tout d&eacute;plier');                
+      } else {
+        $(this).html('Tout replier');        
+      }
     });
   });
 </script>
 
 <div id='collection-tree'>
 <h3 id="titre-arbo">Arborescence du corpus</h3>
+<span style="float:right;clear:both;" class='tout'>Tout d&eacute;plier</span><br />
 <?php  echo $tree; ?>
 </div>
 <?php echo foot(); ?>
